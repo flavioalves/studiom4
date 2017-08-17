f0fb1e7021bfdf2d25cedce65f1eaf2b3368ff5d
Álvaro Fernando <alvarofernandoms@gmail.com>
2017-08-17 16:04:34 -0300

    add course link with each studants form

--- app/assets/javascripts/studant-form.js
+++ app/assets/javascripts/studant-form.js
@@ -2,3 +2,17 @@ $(document)
   .ready(function () {
     $('.cpf-mask').mask('999.999.999-99');
   });
+$(document).on('ajax:success', '.new_studant', function (e, data) {
+  if (data.status == 'arquitetura-na-pratica') {
+    alert(data.message);
+    document.getElementById("pagseguro-arquitetura-na-pratica").click();
+  }
+  else if (data.status == 'pacote') {
+    alert(data.message);
+    document.getElementById("pagseguro-pacote").click();
+  }
+  else if (data.status == 'ex-aluno-iluminacao') {
+    alert(data.message);
+    document.getElementById("close-modal").click();
+  }
+});
--- app/controllers/studants_controller.rb
+++ app/controllers/studants_controller.rb
@@ -4,8 +4,12 @@ class StudantsController < ApplicationController
 
     if studant.nil?
       render json: { status: 'error' }
-    else
-      render json: { status: 'success' }
+    elsif studant.studant_type == 'arquitetura-na-pratica'
+      render json: { status: 'arquitetura-na-pratica', message: "Olá, #{studant.name}! Obrigada pela inscrição! Você está sendo redirecionando para o PagSeguro para prosseguir com o pagamento. Clique em OK para continuar!" }
+    elsif studant.studant_type == 'pacote'
+      render json: { status: 'pacote', message: "Olá, #{studant.name}! Obrigada pela inscrição! Você está sendo redirecionando para o PagSeguro para prosseguir com o pagamento. Clique em OK para continuar!" }
+    elsif studant.studant_type == 'ex-aluno-iluminacao'
+      render json: { status: 'ex-aluno-iluminacao', message: "Olá, #{studant.name}! Obrigada por se inscrever! Vamos fazer a conferência da sua matrícula com a Arqflex e retornamos em até 48horas, por e-mail para que você possa fazer seu pagamento." }
     end
   end
 end
