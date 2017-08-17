e3f7bf82eb3b75d5288c9e1a2251dcf6ce56a35b
Álvaro Fernando <alvarofernandoms@gmail.com>
2017-08-17 18:05:47 -0300

    minor text fix

--- app/controllers/studants_controller.rb
+++ app/controllers/studants_controller.rb
@@ -5,9 +5,9 @@ class StudantsController < ApplicationController
     if studant.nil?
       render json: { status: 'error' }
     elsif studant.studant_type == 'arquitetura-na-pratica'
-      render json: { status: 'arquitetura-na-pratica', message: "Olá, #{studant.name}! Obrigada pela inscrição! Você está sendo redirecionando para o PagSeguro para prosseguir com o pagamento. Clique em OK para continuar!" }
+      render json: { status: 'arquitetura-na-pratica', message: "Olá, #{studant.name}! Obrigada pela inscrição! Você será redirecionado para o PagSeguro para prosseguir com o pagamento. Clique em OK para continuar!" }
     elsif studant.studant_type == 'pacote'
-      render json: { status: 'pacote', message: "Olá, #{studant.name}! Obrigada pela inscrição! Você está sendo redirecionando para o PagSeguro para prosseguir com o pagamento. Clique em OK para continuar!" }
+      render json: { status: 'pacote', message: "Olá, #{studant.name}! Obrigada pela inscrição! Você será redirecionado para o PagSeguro para prosseguir com o pagamento. Clique em OK para continuar!" }
     elsif studant.studant_type == 'ex-aluno-iluminacao'
       render json: { status: 'ex-aluno-iluminacao', message: "Olá, #{studant.name}! Obrigada por se inscrever! Vamos fazer a conferência da sua matrícula com a Arqflex e retornamos em até 48horas, por e-mail para que você possa fazer seu pagamento." }
     end
